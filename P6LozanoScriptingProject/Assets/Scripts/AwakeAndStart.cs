fb59879c8b65478edf540a82e53e3ea14e09df5f
camerashrimp <731406@student.dusd.net>
2026-01-08 12:40:22 -0800

    Awake and start
    new script

---
+++ P6LozanoScriptingProject/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake is Running.");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start is Running.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
